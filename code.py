685d87134c75cfbf14d1320bda24abf0c9c9dc1d
Mysterious-Owl <66870959+Mysterious-Owl@users.noreply.github.com>
2021-02-23 16:27:28 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,5 @@
 from twython import Twython
 from twython import TwythonStreamer
-import configparser
-
 
 class MyStreamer(TwythonStreamer):
     def on_success(self, data):
@@ -12,13 +10,10 @@ class MyStreamer(TwythonStreamer):
             st.update_status(status='Nice Tweet @' + username, in_reply_to_status_id=id)
             print("https://twitter.com/" + username + "/status/" + str(id))
 
-
-config = configparser.ConfigParser()
-config.read('config.ini')
-api_key = config['keys']['api_key']
-api_secret_key = config['keys']['api_secret_key']
-access_token = config['keys']['access_token']
-access_secret_token = config['keys']['access_secret_token']
+api_key = "###"
+api_secret_key = "###"
+access_token = "###"
+access_secret_token = "###"
 
 api = MyStreamer(api_key, api_secret_key, access_token, access_secret_token)
 st = Twython(api_key, api_secret_key, access_token, access_secret_token)
